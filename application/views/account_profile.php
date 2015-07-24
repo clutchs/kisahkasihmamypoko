9a7958aaffcaf2d70a5ae3ca39b53e33b103c891
Defrian Yarfi <defrian.yarfi@gmail.com>
2015-07-24 23:33:34 +0700

    account_profile.php

--- application/views/account_profile.php
+++ application/views/account_profile.php
@@ -13,9 +13,12 @@
 					$thumb		= $pathinfo['filename'].'_thumb.'.$pathinfo['extension'];
 				?>
 				<div class="text-center">
-				<a href="javascript:;" class="popover_bootbox img-thumbnail" rel="<?=base_url();?>uploads/gallery/<?=$this->participant->file_name?>">
+				<a href="javascript:;" class="popover_bootbox img-thumbnail" rel="<?=base_url();?>uploads/gallery/<?=$this->participant->file_name?>" title="<?php echo $this->participant->name; echo ($this->participant->baby_name) ? ' & '.$this->participant->baby_name : ''?>">
 					<img style="height:190px" data-toggle="modal" data-target=".bs-example-modal-sm-<?php echo $image->id ?>" src="<?=base_url();?>uploads/gallery/<?=$thumb?>" alt="<?=$image->file_name;?>" />
 				</a>
+				<div class="img-description hidden">
+                    <?php echo $this->participant->about;?>
+                </div>
 				</div>
 				<?php } ?>
 				<br/><br/>Silahkan Mamy kunjungi galeri <a href="<?php echo base_url('photo_gallery');?>" title="Galeri Kisah Kasih Mamypoko"><strong>Kisah Kasih Mamypoko</strong></a>
