ef75e36c3d8c700e3dc5e220007aaa31d71d1f38
clutchs <muh.hilman@gmail.com>
2015-07-28 18:04:22 +0700

    viewport

--- application/views/template/public/template.php
+++ application/views/template/public/template.php
@@ -3,6 +3,11 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <title><?php echo $page_title; echo config_item('title_default') ? ' | '.config_item('title_default') : '';?></title>
     <!-- Bootstrap Core CSS -->
     <link href="<?php echo base_url();?>assets/public/css/bootstrap.min.css" rel="stylesheet" type="text/css">
